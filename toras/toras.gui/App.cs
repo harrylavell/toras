102621a155f02c29440b4a5df977134f733f3f73
Harry Lavell <hallan@outlook.com.au>
2019-07-26 11:39:22 +1000

    Entry Point Replaced

---
+++ toras/toras.gui/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toras.gui
+{
+    public class App : System.Windows.Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+            app.Run();
+        }
+    }
+}
